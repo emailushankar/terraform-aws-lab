e48267fb0946e7e194562e3dcf245c7c973e5238
Umashankar <emailu.shankar@gmail.com>
2026-01-11 14:14:54 +0530

    Initial Terraform setup with remote state

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+ region = "ap-south-1"
+ profile = "terraform-user"
+ }
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "terraform-web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
